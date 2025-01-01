60504472a8e9de5423d040e71e5a1a0332996432
KahjuksEi <39082880+KahjuksEi@users.noreply.github.com>
2025-01-01 20:07:32 +0300

    Init

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    country: z.string().optional(),
+    rating: z.number().optional(),
+    distillery: z.string().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { blog };
